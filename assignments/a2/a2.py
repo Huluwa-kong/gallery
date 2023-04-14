94773c790463755dfca0bf86b46ab9d57a4810bd
Huluwa-kong <codetutor@foxmail.com>
2023-04-14 08:40:16 +0800

    a2

---
+++ assignments/a2/a2.py
@@ -0,0 +1,13 @@
+from a2_support import *
+
+
+# Implement your classes here
+
+def main():
+    # Implement this only once you've finished and tested ALL of the required
+    # classes.
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ assignments/readme.md
@@ -0,0 +1,5 @@
+若您有关于此文件夹下任何作业的任何需求，可添加vx：codingtutor 或者qq：122929048
+
+更多介绍，请访问
+1. [个人网站](https://www.vipcshelper.com/)
+2. [github](https://github.com/Huluwa-kong)
