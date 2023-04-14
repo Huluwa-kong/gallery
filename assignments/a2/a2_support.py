0c037b3754439809e14da8fcc8b85269e31a05c4
Huluwa-kong <codetutor@foxmail.com>
2023-04-14 08:41:17 +0800

    a2

--- assignments/a2/a2_support.py
+++ assignments/a2/a2_support.py
@@ -1,3 +1,7 @@
+# 若您有关于此文件夹下任何作业的任何需求，可添加vx：codingtutor 或者qq：122929048
+# 若您有关于此文件夹下任何作业的任何需求，可添加vx：codingtutor 或者qq：122929048
+# 若您有关于此文件夹下任何作业的任何需求，可添加vx：codingtutor 或者qq：122929048
+
 import random
 random.seed(10012023)
 
