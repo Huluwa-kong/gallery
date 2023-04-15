7d97e4757426795bb38fd9ac401218cb039a1872
Brye <69114816+Huluwa-kong@users.noreply.github.com>
2023-04-15 13:53:29 +0800

    Update a2.py

--- assignments/a2/a2.py
+++ assignments/a2/a2.py
@@ -1,3 +1,8 @@
+# 若您有关于此文件夹下任何作业的任何需求，可添加vx：codingtutor 或者qq：122929048
+# 若您有关于此文件夹下任何作业的任何需求，可添加vx：codingtutor 或者qq：122929048
+# 若您有关于此文件夹下任何作业的任何需求，可添加vx：codingtutor 或者qq：122929048
+
+
 from a2_support import *
 
 
